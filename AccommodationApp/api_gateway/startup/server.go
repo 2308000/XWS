1b193d46a79eaf31ab26c321d3ec5bb57dc40126
Nikola <nikolavukic333@gmail.com>
2023-06-17 13:50:08 +0200

    double grade-service bugfix

--- AccommodationApp/api_gateway/startup/server.go
+++ AccommodationApp/api_gateway/startup/server.go
@@ -9,7 +9,6 @@ import (
 	userGw "accommodation_booking/common/proto/user_service"
 	"context"
 	"fmt"
-	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
