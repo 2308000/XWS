dd1a82b5a607fc92f1edd56b606b31ba170e5885
Vukić Nikola <nikolavukic333@gmail.com>
2023-06-19 14:46:46 +0200

    cors bugfix attempt 1 (#64)

--- AccommodationApp/api_gateway/startup/server.go
+++ AccommodationApp/api_gateway/startup/server.go
@@ -68,6 +68,10 @@ func (server *Server) Start() {
 
 func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//w.Header().Set("Access-Control-Allow-Origin", "*")
+		//w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		//w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 		h := w.Header()
 
 		if r.Header.Get("Origin") != "" {
